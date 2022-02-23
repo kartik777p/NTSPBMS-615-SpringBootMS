202d8a61ffabb144a032917958e92326bbb4528a
kartik777p <kartik777p@gmail.com>
2022-02-23 15:29:06 +0530

    CollectionsMapping lazy Loading problem solved 2 ways.. 1)Using additional cfgs in props file 2)by Enable eager loading using @LazyCollection

--- BootDataJPAProj16-CollectionsMapping/src/main/java/com/nit/entity/Employee.java
+++ BootDataJPAProj16-CollectionsMapping/src/main/java/com/nit/entity/Employee.java
@@ -17,6 +17,8 @@ import javax.persistence.MapKeyColumn;
 import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 import org.hibernate.annotations.ListIndexBase;
 
 import lombok.AllArgsConstructor;
@@ -45,17 +47,21 @@ public class Employee implements Serializable {
     @OrderColumn(name="FRIEND_INDEX")															//Create Column name to Store index IN Child table (Applicable only on List type)
     @ListIndexBase(value = 1)																						//Start endex From 1(Applicable only on List type)
    @Column(name="FRIENDS_NAME",length = 20)                                             //Create  FRIENDS_NAME Column in Parent table to store names
+    //2nd way
+    @LazyCollection(LazyCollectionOption.FALSE)  //Along with parent table child table should be loaded so no need of any cfgs in Application.properties
     private  List<String> friends;
     
     //SET TYPE 
     @ElementCollection 																									//to enable collections mapping														
     @CollectionTable(name="EMPLOYEE_CONTACT_DETAILS", joinColumns = @JoinColumn(name="EMP_ID",referencedColumnName = "EID"))
     @Column(name="PHONE_NUMBERS",length = 11)                                       //Create  Phone_Numbers Column in Parent table to store phone_Numbers
+    @LazyCollection(LazyCollectionOption.FALSE)  //Along with parent table child table should be loaded so no need of any cfgs in Application.properties
     private Set<Long> phoneNumbers;
     
     @ElementCollection 					                                                       //to enable collections mapping
     @CollectionTable(name="EMPLOYEE_ID_DETAILS", joinColumns = @JoinColumn(name="EMP_ID",referencedColumnName = "EID"))
     @MapKeyColumn(name = "CARD_TYPE",length = 20)                               //Create column in Child table of Card_Type to Hold key
     @Column(name="CARD_NUMBERS",length = 20)                            //Create column in Child table of CARD_NUMBER to Hold VALUES
+    @LazyCollection(LazyCollectionOption.FALSE)  //Along with parent table child table should be loaded so no need of any cfgs in Application.properties
     private Map<String,String> idDetails;
 }
