61b755264176d7434e5bbaac2e0925acca2a0d2a
kartik777p <kartik777p@gmail.com>
2022-03-12 20:16:32 +0530

    First MVC Application Which can Run as a Server or StandAlone

--- BootMVCProj01-FirstApp/src/main/java/com/nit/BootMvcProj01FirstAppApplication.java
+++ BootMVCProj01-FirstApp/src/main/java/com/nit/BootMvcProj01FirstAppApplication.java
@@ -3,7 +3,7 @@ package com.nit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  
 public class BootMvcProj01FirstAppApplication {
 
 	public static void main(String[] args) {
